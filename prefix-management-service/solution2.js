7495b82a46efa3f6c877a37dce8bd2f7b0fe75a7
DhulipudiAchyutSaiKrishna <63388971+DhulipudiAchyutSaiKrishna@users.noreply.github.com>
2022-12-30 21:13:30 +0530

    image commit for solution1(forgot about the image)

--- prefix-management-service/solution2.js
+++ prefix-management-service/solution2.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 
 router.get('/', (req, res) => {
+    
     res.send('Solution2 is working');
 });
 
